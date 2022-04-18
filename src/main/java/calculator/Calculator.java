ebbcf1b3792c5896c2e8e2f088423f312c9a86f4
siddhardh <siddhardhrahul905@gmail.com>
2022-04-18 12:37:46 +0530

    Changed Calculator: Added division and multiplication

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -19,7 +19,8 @@ public class Calculator {
         do {
             System.out.println("Calculator-DevOps, Choose to perform operation");
             System.out.print("Press 1 to Add\nPress 2 to Subtract\n" +
-                    "Press any other key to exit\nEnter your choice: ");
+                    "Press 3 to Multiply\nPress 4 to Divide\n" +
+                    "Press any other key to exit\n\tEnter your choice: ");
             int choice;
             try {
                 choice = scanner.nextInt();
@@ -27,9 +28,9 @@ public class Calculator {
                 return;
             }
             try {
-                System.out.print("Enter the first number : ");
+                System.out.print("\tEnter the first number : ");
                 number1 = scanner.nextDouble();
-                System.out.print("Enter the second number : ");
+                System.out.print("\tEnter the second number : ");
                 number2 = scanner.nextDouble();
             } catch (InputMismatchException error) {
                 System.out.println("Invalid input, Entered input is not a number");
@@ -43,6 +44,12 @@ public class Calculator {
                 case 2:
                     // do subtraction
                     System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
+                    break;
+                case 3:
+                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
+                    break;
+                case 4:
+                    System.out.println("Division result is : " + calculator.divide(number1, number2));
                     break;
                 default:
                     System.out.println("Exiting....");
@@ -67,4 +74,21 @@ public class Calculator {
         logger.info("Executing subtract function!");
         return result;
     }
+
+    public double multiply(double number1, double number2) {
+        System.out.println("Multiplication of " + number1 + ", " + number2);
+        double result = number1 * number2;
+        System.out.println("RESULT = " + result);
+        logger.info("Executing multiply function!");
+        return result;
+    }
+
+    public double divide(double number1, double number2) {
+        System.out.println("Division of " + number1 + ",by " + number2);
+        double result = number1/number2;
+        System.out.println("RESULT = " + result);
+        logger.info("Executing divide function!");
+        return result;
+    }
+
 }
