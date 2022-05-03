027546743d358e4809049e1f0568c570b73e648c
siddhardh <siddhardhrahul905@gmail.com>
2022-05-03 16:03:47 +0530

    Changed log4j2.xml file and added new test case for multiplication and division

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -32,4 +32,30 @@ public class CalculatorTest {
         assertNotEquals("Subtracting two integer numbers for False Positive", 6, calculator.subtract(2, 2), DELTA);
         assertNotEquals("Subtracting two double numbers for False Positive", -7.3, calculator.subtract(2.1, 3.2), DELTA);
     }
+
+    @Test
+    public void multiplyingTruePositive(){
+        assertEquals("Multiplying two integer numbers for True Positive", 15, calculator.multiply(5, 3), DELTA);
+        assertEquals("Multiplying two double numbers for True Positive", 5.5, calculator.multiply(2.5, 2.2), DELTA);
+    }
+
+    @Test
+    public void multiplyingFalsePositive(){
+        assertNotEquals("Multiplying two integer numbers for False Positive", 18, calculator.multiply(5, 3), DELTA);
+        assertNotEquals("Multiplying two double numbers for False Positive", 9.5, calculator.multiply(2.5, 2.2), DELTA);
+    }
+//
+    @Test
+    public void dividingTruePositive(){
+        assertEquals("Dividing two integer numbers for True Positive", 2, calculator.divide(10, 5), DELTA);
+        assertEquals("Dividing two double numbers for True Positive", 2.25, calculator.divide(5.4, 2.4), DELTA);
+    }
+
+    @Test
+    public void dividingFalsePositive(){
+        assertNotEquals("Dividing two integer numbers for False Positive", 7, calculator.divide(10, 5), DELTA);
+        assertNotEquals("Dividing two double numbers for False Positive", 1.95, calculator.divide(5.4, 2.4), DELTA);
+    }
+
+
 }
